911aca59f42d7644f3a54ba7f9ef8ce5add45ea8
Lee Edwards <leeredwards@gmail.com>
2022-12-07 18:04:21 -0800

    WIP show pages work

--- pages/[[...token]].tsx
+++ pages/[[...token]].tsx
@@ -20,36 +20,45 @@ const isIndexPage = (token: string | null) => !token || token === "index.html";
 export default function Gallery() {
   const router = useRouter();
   const [photoUrls, setPhotoUrls] = useState<string[] | null>([]);
-  const token: string | null = router.query.token ? router.query.token[0] : null;
+  const [token, setToken] = useState<string | null>([]);
 
   useEffect(() => {
-    function fetchPhotos(prefix: string) {
-      const albumPhotosKey: string = prefix + "/";
-
-      const objs = s3.listObjects({ Bucket: bucketName, Prefix: albumPhotosKey }, function(err, data) {
-        if (err) {
-          return alert('There was an error viewing your album: ' + err.message);
-        }
-        // 'this' references the AWS.Request instance that represents the response
-        // @ts-ignore eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        var href = this.request.httpRequest.endpoint.href;
-        var bucketUrl = href + bucketName + '/';
-        var photos = data.Contents!.map((photo) => `${bucketUrl}${photo.Key!}`);
-
-        if (isIndexPage(token)) {
-          photos = photos.reverse();
-        } else {
-          const tokenIndex = photos[1].split("/").length - 2; // this is the same for every url and can be done once
-          photos = photos.filter((u: string) => u.split("/")[tokenIndex] == token);
-        }
-
-        setPhotoUrls(photos);
-      });
-
-      return objs;
+    if (router.isReady) {
+      console.log("SETTING TOKEN", router.query.token ? router.query.token[0] : "(undefined)");
+      setToken(router.query.token ? router.query.token[0] : null);
     }
+  }, [router.query.token]);
 
-    fetchPhotos("output");
+  useEffect(() => {
+    if (router.isReady && token) {
+      function fetchPhotos(prefix: string) {
+        const objs = s3.listObjects({ Bucket: bucketName, Prefix: prefix }, function(err, data) {
+          if (err) {
+            return alert('There was an error viewing your album: ' + err.message);
+          }
+          // 'this' references the AWS.Request instance that represents the response
+          // @ts-ignore eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          var href = this.request.httpRequest.endpoint.href;
+          var bucketUrl = href + bucketName + '/';
+          var photos = data.Contents!.map((photo) => `${bucketUrl}${photo.Key!}`);
+
+          if (isIndexPage(token)) {
+            photos = photos.reverse();
+          }
+
+          photos = photos.filter(p => {  // filter out root bucket/prefix urls
+            const filenameInParts = p.split(".");
+            return filenameInParts[filenameInParts.length - 1] == "png"
+          });
+
+          setPhotoUrls(photos);
+        });
+
+        return objs;
+      }
+
+      fetchPhotos("output/" + token);
+    }
   }, [token]);
 
   return (
